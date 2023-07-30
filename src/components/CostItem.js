7a07456b2109f45100166a118a29a1badba94572
gavabatya <79111180057@yandex.ru>
2023-07-30 18:57:06 +0300

    add .css, add dynamic jsx

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,10 +1,15 @@
+import "./CostItem.css";
+
 function CostItem() {
+  const costDate = new Date(2023, 2, 12);
+  const costDiscription = "Холодильник";
+  const costAmount = 999.99;
   return (
-    <div>
-      <div>12.03.2023</div>
-      <div>
-        <h2>Холодильник</h2>
-        <div>$999</div>
+    <div className="cost-item">
+      <div>{costDate.toDateString()}</div>
+      <div className="cost-item__description">
+        <h2>{costDiscription}</h2>
+        <div className="cost-item__price">${costAmount}</div>
       </div>
     </div>
   );
